6c52e5d14517da1de77175ee56fb9182fbade959
Gervasio Junior <jamilservicos@gmail.com>
2024-06-12 08:36:33 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run with root"
-  exit 1
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run with root"
+    exit 1
 fi
 
-chmod +x installers/*.sh
-bash installers/firewalld.sh
-bash installers/docker-ce.sh
-bash installers/fail2ban.sh
+if ! [ -d "./installers" ]; then
+    wget https://github.com/jamilservicos/vps-utils-stater/archive/refs/heads/main.zip -O vps-utils-stater.zip
+    if test -x "$(which firewall-cmd)"; then
+        apt install unzip -y
+    fi
+    unzip -q vps-utils-stater.zip
+    mv vps-utils-stater-main/installers .
+    mv vps-utils-stater-main/files .
+    rm -rf vps-utils-stater*
+fi
+
+if ! [ -d "./installers" ]; then
+    chmod x installers/*.sh
+    bash installers/firewalld.sh
+    bash installers/docker-ce.sh
+    bash installers/fail2ban.sh
+fi
